1354dedf3d0693ecafe7085cbcb45dca5a85a8dd
Joe <joe@mockjenkins.my.infra>
2021-02-15 20:31:39 -0500

    updated to local storage

--- main.tf
+++ main.tf
@@ -1,9 +1,7 @@
 # Terraform state will be stored in S3
 terraform {
-  backend "s3" {
-    bucket = "terraform-bucket-alex"
-    key    = "terraform.tfstate"
-    region = "us-east-2"
+  backend "local" {
+    path = "terraform.tfstate" 
   }
 }
 
